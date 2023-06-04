7b81f3971038a4e82712663d3ab51d329576497d
Anderson Murillo Tito Lira Leite <ra248221@students.ic.unicamp.br>
2023-06-04 15:30:36 -0300

    My first React App finished!!!

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import { Component } from 'react';
+import React from "react";
+import { Component } from "react";
+import { useState } from "react";
 
 class ErrorBoundry extends Component {
     constructor(props) {
         super(props);
-        this.state = {
+        this.state = { // contem dados e informações sobre o estado do componente
+            // ErrorBoundry
             hasError: false,
         }
     }
 
-    componentDidCatch(error, info) { // try catch do java, javascript
+    componentDidCatch(error, info) {
         this.setState({ hasError: true});
     }
 
     render() {
-        if (this.state.hasError) {
-            return <h1>Oops. That is not good</h1>
-        }
+        if (this.state.hasError)
+            return <h1>Ops! The page is broken...</h1>
         return this.props.children;
     }
 }
 
 export default ErrorBoundry;
+
+
+/*function ErrorBoundry() {
+    const [hasError, setHasError] = useState(false);
+
+    if (hasError) {
+        setHasError(true);
+    }
+    return (
+        <div>
+
+        </div>
+    );
+}*/
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from '../components/SearchBox';
 import Scroll from "../components/Scroll";
@@ -6,47 +6,62 @@ import ErrorBoundry from "../components/ErrorBoundry";
 // o robots esta exportando um array(varias coisas), entao ele fica apenas export e
 // precisa estar entre chave
 
-class App extends Component { // container: smart component
-    constructor() {
+function App() { // container: smart component
+    /*constructor() {
         super();
         this.state = {
             robots: [],
             searchfield: '',
         }
     }
-
+    
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
         .then(users => this.setState({ robots: users}));
+    }*/
+
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchField] = useState('');
+    const [count, setCount] = useState(0);
+
+    useEffect( () => { // o useEffect roda sempre que o App renderiza
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => setRobots(users));
+        console.log(count);
+    }, [count]); // o segundo argumento do useEffect diz qual variavel que muda e
+    // por isso precisa ser re-renderizada, isso porque o useEffect roda sempre que o App component esta rodando.
+    // Como o robots nao muda, entao o segundo argumento deve ser um array vazio(o segundo argumento sempre eh um array) pois
+    // isso mostra para o useEffect que nenhuma variavel esta mudando e portanto ele nao precisa rodar de novo, ou seja, precisa
+    // rodar apenas uma vez.
+    // Obs: No caso o count precisa ser atualizado, entao ele entra no segundo argumento do useEffect
+
+    const onSearchChange = (event) => {
+        // this.setState({ setSearchField: event.target.value })
+        setSearchField(event.target.value);
     }
 
-    // this is not an lifecycle method of React, so it may have an arrow function
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
-
-    render () {
-        const { robots, searchfield } = this.state
-        const filteredRobots = robots.filter(robot => { // iterar sobre o vetor de robos e para cada robo...
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase()); 
-        });
-        if (!robots.length) {
-            return (
-                <h1 id="title" className="tc f1">Loading...</h1>
-            );
-        } else {
-            return (
-                <div>
-                    <h1 className="tc f1" id="title">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}></SearchBox>
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList robots={filteredRobots}></CardList>
-                        </ErrorBoundry>
-                    </Scroll>
-                </div>
-            );
-        }
+    // const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => { // iterar sobre o vetor de robos e para cada robo...
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase()); 
+    });
+    if (!robots.length) {
+        return (
+            <h1 id="title" className="tc f1">Loading...</h1>
+        );
+    } else {
+        return (
+            <div>
+                <h1 className="tc f1" id="title">RoboFriends</h1>
+                <button onClick={()=>setCount(count + 1)}>Click Me!</button>
+                <SearchBox searchChange={onSearchChange}></SearchBox>
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots}></CardList>
+                    </ErrorBoundry>
+                </Scroll>
+            </div>
+        );
     }
 }
 
